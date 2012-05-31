59153385f2de5576e54bc7882b9f4e6d970d97ee
Sean Young <seany@Seans-MacBook.local>
2012-05-31 16:43:20 +1000

    Revert "message" reverting
    This reverts commit bcb630a8cedc62c0fa651e4168f5558c5719e871.

---
+++ src/visualize.h
@@ -0,0 +1,10 @@
+#ifndef VISUALIZE_H
+#define VISUALIZE_H
+
+extern bool stopAnime;
+extern int simulation_interval;
+
+void handleKeyPress (unsigned char, int , int);
+void handleResize (int , int);
+
+#endif
